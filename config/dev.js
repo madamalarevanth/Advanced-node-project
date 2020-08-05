2dbc772faf424164d502fc60c2fae3a4626b50ee
madamalarevanth <revanthrex1001@gmail.com>
2020-08-05 17:35:45 +0530

    --- Auto-Git Commit ---

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+    '203524929931-p929s7hkjts0lepkubtitnj6439pt878.apps.googleusercontent.com',
+  googleClientSecret: 'ZonqWwRdx0OXguisbQ-bKDBI',
+  mongoURI: 'mongodb+srv://revanth:mnrao123@cluster0.e8pwt.mongodb.net/blog_dev?retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
