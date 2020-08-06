ea668157ba195b3ce4ac65454c797b93eaa28745
madamalarevanth <revanthrex1001@gmail.com>
2020-08-06 14:51:27 +0530

    --- Auto-Git Commit ---

--- services/cache.js
+++ services/cache.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function(){
-    console.log('I am about to run query')
-    console.log(this.getQuery());
-    console.log(this.mongooseCollection.name);
+
+    const key = Object.assign({}, this.getQuery(),{
+        collection: this.mongooseCollection.name 
+    });
+    console.log(key);
     return exec.apply(this,arguments);
 }
