9e80b4ab337f63ff591def46e5096c340be40678
madamalarevanth <revanthrex1001@gmail.com>
2020-08-06 15:21:33 +0530

    --- Auto-Git Commit ---

--- services/cache.js
+++ services/cache.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function(){
+mongoose.Query.prototype.exec = async function(){
 
+    //this key contains an object which contains id 
+    //and the collection name concated to it to create a unique key 
     const key = Object.assign({}, this.getQuery(),{
         collection: this.mongooseCollection.name 
     });
-    console.log(key);
-    return exec.apply(this,arguments);
+    
+    //see if we have a value for key in redis
+    const cacheValue = await client.get(key);
+
+    //if exists return that 
+    if (cacheValue){
+        console.log(cacheValue);
+    }
+
+    //otherwise, issue the query to mongo and store 
+    //result in redis
+    const result = await exec.apply(this,arguments);
+    return result
 }
