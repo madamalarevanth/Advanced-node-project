d6c6495fb5d867274cfdb71173bddc7aa3973e80
madamalarevanth <revanthrex1001@gmail.com>
2020-08-07 12:10:39 +0530

    --- Auto-Git Commit ---

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,15 +1,26 @@
 const puppeteer = require('puppeteer');
 
-test('Adds two numbers',()=>{
-    const sum =1+2;
+let browser,page;
 
-    expect(sum).toEqual(3);
-});
-
-test('We can launch a browser',async ()=>{
-    const browser = await puppeteer.launch({
+beforeEach(async ()=>{
+    browser = await puppeteer.launch({
         headless:false
     });
-    const page = await browser.newPage();
     
-})
+    page = await browser.newPage();
+
+    await page.goto('localhost:3000');
+});
+
+afterEach(async ()=>{
+    await browser.close();
+});
+test('We can launch a browser',async ()=>{
+    const text = await page.$eval('a.brand-logo', el=>el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+});
+
+test('Clicking login start oauth flow',async ()=>{
+    await page.click('.right a');
+});
