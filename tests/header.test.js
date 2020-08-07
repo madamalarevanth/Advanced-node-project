9307ab8f8deb1e0e15cf67f5d192bf84a130c222
madamalarevanth <revanthrex1001@gmail.com>
2020-08-07 15:11:19 +0530

    --- Auto-Git Commit ---

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,40 +1,40 @@
 const puppeteer = require('puppeteer');
 
-let browser,page;
+let browser, page;
 
-beforeEach(async ()=>{
+beforeEach(async () => {
     browser = await puppeteer.launch({
-        headless:false
+        headless: false
     });
-    
+
     page = await browser.newPage();
 
     await page.goto('localhost:3000');
 });
 
-afterEach(async ()=>{
+afterEach(async () => {
     await browser.close();
 });
 
-test('We can launch a browser',async ()=>{
-    const text = await page.$eval('a.brand-logo', el=>el.innerHTML);
+test('We can launch a browser', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
     expect(text).toEqual('Blogster');
 });
 
-test('Clicking login to start Oauth flow', async() => {
+test('Clicking login to start Oauth flow', async () => {
     await page.click('.right a');
     const url = await page.url();
-    
+
     expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test('When signin , shows logout button',async()=>{
+test('When signin , shows logout button', async () => {
     const id = '5f2a9c9f8195b26835d466a1';
     const Buffer = require('safe-buffer').Buffer;
-    const sessionObject ={
-        passport:{
-            user:id
+    const sessionObject = {
+        passport: {
+            user: id
         }
     };
     const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
@@ -43,6 +43,17 @@ test('When signin , shows logout button',async()=>{
     const Keygrip = require('keygrip');
     const keys = require('../config/keys');
     const keygrip = new Keygrip([keys.cookieKey]);
-    const sig = keygrip.sign('session='+sessionString);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    //set the cookies with sessionstring and signature 
+    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session.sig', value: sig });
+    await page.goto('localhost:3000');
+    
+    //wait till all the session variables are set and the anchor tag shows up in the page
+    await page.waitFor('a[href="/auth/logout"]');
+
+    const text = await page.$eval('a[href="/auth/logout"]',el=> el.innerHTML);
+    expect(text).toEqual('Logout');
     
 });
